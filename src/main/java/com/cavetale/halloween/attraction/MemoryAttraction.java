0ac2a8cde6b97ed6f7342489bb807315683033ed
StarTux <ths.loy@googlemail.com>
2022-10-25 17:04:48 +0200

    Implement the Memory attraction

--- src/main/java/com/cavetale/halloween/attraction/MemoryAttraction.java
+++ src/main/java/com/cavetale/halloween/attraction/MemoryAttraction.java
@@ -1,25 +1,29 @@
 package com.cavetale.halloween.attraction;
 
 import com.cavetale.area.struct.Area;
-import com.cavetale.core.font.Unicode;
+import com.cavetale.core.event.hud.PlayerHudEvent;
+import com.cavetale.core.event.hud.PlayerHudPriority;
 import com.cavetale.core.struct.Vec3i;
+import com.cavetale.mytems.Mytems;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import net.kyori.adventure.bossbar.BossBar;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
-import static net.kyori.adventure.text.Component.join;
 import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.JoinConfiguration.noSeparators;
-import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 public final class MemoryAttraction extends Attraction<MemoryAttraction.SaveTag> {
-    protected static final Duration PLAY_TIME = Duration.ofSeconds(60);
+    protected static final Duration PLAY_TIME = Duration.ofSeconds(90);
+    protected static final int REVEAL_TICKS = 30;
     protected List<Vec3i> memoryBlocks = new ArrayList<>();
     protected int secondsLeft;
+    protected int maxTries;
 
     protected MemoryAttraction(final AttractionConfiguration config) {
         super(config, SaveTag.class, SaveTag::new);
@@ -28,9 +32,9 @@ public final class MemoryAttraction extends Attraction<MemoryAttraction.SaveTag>
                 memoryBlocks.addAll(cuboid.enumerate());
             }
         }
-        this.displayName = text("Memory Game", DARK_RED);
-        this.description = text("Choose one of my chests and keep"
-                                          + " what you find inside!");
+        this.displayName = booth.format("Memory Game");
+        this.description = text("Find matching pairs of blocks. Reveal 2 blocks at a time.");
+        this.maxTries = memoryBlocks.size();
     }
 
     @Override
@@ -41,10 +45,10 @@ public final class MemoryAttraction extends Attraction<MemoryAttraction.SaveTag>
     @Override
     public void start(Player player) {
         saveTag.currentPlayer = player.getUniqueId();
-        placeBlocks();
+        generateBlocks();
         saveTag.gameStarted = System.currentTimeMillis();
         startingGun(player);
-        changeState(State.GAME);
+        changeState(State.PICK_1);
     }
 
     @Override
@@ -60,7 +64,7 @@ public final class MemoryAttraction extends Attraction<MemoryAttraction.SaveTag>
 
     @Override
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (saveTag.state != State.GAME && saveTag.state != State.GAME) return;
+        if (!isPlaying()) return;
         switch (event.getAction()) {
         case RIGHT_CLICK_BLOCK:
         case LEFT_CLICK_BLOCK:
@@ -70,13 +74,44 @@ public final class MemoryAttraction extends Attraction<MemoryAttraction.SaveTag>
         Player player = getCurrentPlayer();
         if (player == null) return;
         if (!event.getPlayer().equals(player)) return;
-        Vec3i clickedBlock = Vec3i.of(event.getClickedBlock());
-        for (Vec3i vec : memoryBlocks) {
-            if (clickedBlock.equals(vec)) {
-                event.setCancelled(true);
-                // TODO
-                return;
+        final Block block = event.getClickedBlock();
+        final Vec3i clickedBlock = Vec3i.of(event.getClickedBlock());
+        final int clickedIndex = memoryBlocks.indexOf(clickedBlock);
+        if (clickedIndex < 0) return;
+        final Material clickedMaterial = saveTag.hiddenBlocks.get(clickedIndex);
+        final boolean clickedAlreadyFound = saveTag.foundBlocks.get(clickedIndex);
+        if (clickedAlreadyFound) return;
+        if (saveTag.state == State.PICK_1) {
+            block.setBlockData(clickedMaterial.createBlockData(), false);
+            confetti(player, block.getRelative(event.getBlockFace()).getLocation().add(0.5, 0.5, 0.5));
+            saveTag.revealedIndex = clickedIndex;
+            changeState(State.PICK_2);
+        } else if (saveTag.state == State.PICK_2) {
+            if (saveTag.revealedIndex == clickedIndex) return;
+            block.setBlockData(clickedMaterial.createBlockData(), false);
+            confetti(player, block.getRelative(event.getBlockFace()).getLocation().add(0.5, 0.5, 0.5));
+            if (clickedMaterial == saveTag.hiddenBlocks.get(saveTag.revealedIndex)) {
+                saveTag.foundBlocks.set(clickedIndex, true);
+                saveTag.foundBlocks.set(saveTag.revealedIndex, true);
+                progress(player);
+                saveTag.complete = true;
+                for (boolean v : saveTag.foundBlocks) {
+                    if (!v) {
+                        saveTag.complete = false;
+                        break;
+                    }
+                }
+                if (saveTag.complete) {
+                    perfect(player);
+                    prepareReward(player, true);
+                    plugin.sessionOf(player).setCooldown(this, completionCooldown);
+                } else {
+                    changeState(State.PICK_1);
+                }
+            } else {
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_SNARE, SoundCategory.MASTER, 1.0f, 0.5f);
             }
+            changeState(State.REVEAL);
         }
     }
 
@@ -84,45 +119,85 @@ public final class MemoryAttraction extends Attraction<MemoryAttraction.SaveTag>
         changeState(State.IDLE);
     }
 
-    private final List<Material> hiddenMaterials = List.of(Material.PUMPKIN,
-                                                           Material.CARVED_PUMPKIN,
-                                                           Material.JACK_O_LANTERN);
+    private final List<Material> hiddenMaterials = List
+        .of(Material.PUMPKIN,
+            Material.MELON,
+            Material.DIAMOND_ORE,
+            Material.GOLD_ORE,
+            Material.IRON_ORE,
+            Material.COPPER_ORE,
+            Material.GOLD_BLOCK,
+            Material.EMERALD_BLOCK,
+            Material.REDSTONE_BLOCK,
+            Material.DIAMOND_BLOCK);
 
-    private void placeBlocks() {
+    private void generateBlocks() {
+        List<Material> hidden = new ArrayList<>();
+        hidden.addAll(hiddenMaterials);
+        Collections.shuffle(hidden);
         int count = 0;
         for (Vec3i vec : memoryBlocks) {
             vec.toBlock(world).setType(Material.SMOOTH_STONE);
         }
-        saveTag.hiddenBlocks = new ArrayList<>(memoryBlocks.size());
-        for (int i = 0; i < memoryBlocks.size(); i += 2) {
-            Material mat = hiddenMaterials.get((i / 2) % hiddenMaterials.size());
+        saveTag.hiddenBlocks.clear();
+        saveTag.foundBlocks.clear();
+        for (int i = 0; i < memoryBlocks.size() / 2; i += 1) {
+            Material mat = hidden.get(i);
             saveTag.hiddenBlocks.add(mat);
             saveTag.hiddenBlocks.add(mat);
+            saveTag.foundBlocks.add(false);
+            saveTag.foundBlocks.add(false);
         }
         Collections.shuffle(saveTag.hiddenBlocks);
     }
 
-    private void clearBlocks() {
-        saveTag.hiddenBlocks = null;
+    private void hideBlocks() {
+        for (int i = 0; i < memoryBlocks.size(); i += 1) {
+            Vec3i vector = memoryBlocks.get(i);
+            boolean found = saveTag.foundBlocks.get(i);
+            Block block = vector.toBlock(world);
+            final Material material = found
+                ? saveTag.hiddenBlocks.get(i)
+                : Material.SMOOTH_STONE;
+            block.setBlockData(material.createBlockData(), false);
+        }
     }
 
-    protected State tickGame(boolean second) {
-        Player player = getCurrentPlayer();
-        if (player == null) return State.IDLE;
-        long now = System.currentTimeMillis();
-        long timeout = saveTag.gameStarted + PLAY_TIME.toMillis();
-        if (now > timeout) {
-            timeout(player);
-            return State.IDLE;
+    private void clearBlocks() {
+        for (int i = 0; i < memoryBlocks.size(); i += 1) {
+            Vec3i vector = memoryBlocks.get(i);
+            Block block = vector.toBlock(world);
+            block.setType(Material.AIR);
         }
-        int seconds = (int) ((timeout - now - 1L) / 1000L) + 1;
-        if (seconds != secondsLeft) {
-            secondsLeft = seconds;
-            player.sendActionBar(join(noSeparators(),
-                                      text(Unicode.WATCH.string + seconds, GOLD),
-                                      text(" Pick a chest!", WHITE)));
-            for (Vec3i vec : memoryBlocks) {
-                highlight(player, vec.toLocation(player.getWorld()).add(0, 0.5, 0));
+        saveTag.hiddenBlocks.clear();
+        saveTag.foundBlocks.clear();
+        saveTag.revealedIndex = -1;
+        saveTag.revealTicks = 0;
+        saveTag.complete = false;
+    }
+
+    protected State tickGame() {
+        final Player player = getCurrentPlayer();
+        if (player == null) return State.IDLE;
+        final long now = System.currentTimeMillis();
+        final long timeout = saveTag.gameStarted + PLAY_TIME.toMillis();
+        if (!saveTag.complete) {
+            if (now > timeout) {
+                timeout(player);
+                return State.IDLE;
+            }
+        }
+        secondsLeft = (int) ((timeout - now - 1L) / 1000L) + 1;
+        if (saveTag.state == State.REVEAL) {
+            saveTag.revealTicks += 1;
+            if (saveTag.revealTicks >= REVEAL_TICKS) {
+                if (saveTag.tries >= maxTries) {
+                    fail(player);
+                    return State.IDLE;
+                }
+                return saveTag.complete
+                    ? State.IDLE
+                    : State.PICK_1;
             }
         }
         return null;
@@ -136,10 +211,34 @@ public final class MemoryAttraction extends Attraction<MemoryAttraction.SaveTag>
     }
 
     enum State {
-        IDLE,
-        GAME {
+        IDLE {
+            @Override protected void enter(MemoryAttraction instance) {
+                instance.clearBlocks();
+                instance.saveTag.tries = 0;
+            }
+        },
+        PICK_1 {
             @Override protected State tick(MemoryAttraction instance) {
-                return instance.tickGame(false);
+                return instance.tickGame();
+            }
+            @Override protected void enter(MemoryAttraction instance) {
+                instance.saveTag.complete = false;
+                instance.saveTag.revealedIndex = -1;
+                instance.hideBlocks();
+            }
+        },
+        PICK_2 {
+            @Override protected State tick(MemoryAttraction instance) {
+                return instance.tickGame();
+            }
+        },
+        REVEAL {
+            @Override protected State tick(MemoryAttraction instance) {
+                return instance.tickGame();
+            }
+            @Override protected void enter(MemoryAttraction instance) {
+                instance.saveTag.revealTicks = 0;
+                instance.saveTag.tries += 1;
             }
         },
         ;
@@ -156,6 +255,19 @@ public final class MemoryAttraction extends Attraction<MemoryAttraction.SaveTag>
     protected static final class SaveTag extends Attraction.SaveTag {
         protected State state = State.IDLE;
         protected long gameStarted;
-        protected List<Material> hiddenBlocks;
+        protected List<Material> hiddenBlocks = new ArrayList<>();
+        protected List<Boolean> foundBlocks = new ArrayList<>();
+        protected int revealedIndex = -1;
+        protected int revealTicks;
+        protected boolean complete;
+        protected int tries;
+    }
+
+    @Override
+    public void onPlayerHud(PlayerHudEvent event) {
+        event.bossbar(PlayerHudPriority.HIGHEST,
+                      makeProgressComponent(secondsLeft, Mytems.QUESTION_MARK, saveTag.tries, maxTries),
+                      BossBar.Color.RED, BossBar.Overlay.PROGRESS,
+                      (float) secondsLeft / (float) PLAY_TIME.toSeconds());
     }
 }
